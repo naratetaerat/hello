36405ed75d5ec432d5fd4cc7f398614e82a8c6ea
narate <narate@laptop.(none)>
2011-10-10 21:36:15 -0500

    first commit

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+void hello(const std::string & str){
+  std::cout << "Hellllllo " << str << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello.h"
+#include "hi.h"
+
+int main(){
+  hello("ABC");
+  hi("ABC");
+  return 0;
+}
